c1553114ca939ea29f0c90e54f24be2e4f6c1028
Michael Akridge <michael.akridge@noaa.gov>
2024-04-24 09:51:59 -1000

    test 2

--- assets/js/loadReadmes.js
+++ assets/js/loadReadmes.js
@@ -1,21 +1,23 @@
-fetch('packages.json')
-  .then(response => response.json())
-  .then(data => {
-    const navList = document.getElementById('dropdownMenu'); // This ID should refer to the dropdown menu container
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('packages.json')
+    .then(response => response.json())
+    .then(data => {
+        const navList = document.getElementById('dropdownMenu'); // This ID should refer to the dropdown menu container
 
-    data.forEach(pkg => {
-      const parts = pkg.name.split('/'); // Splits the 'name' into parts
-      const folderName = parts[parts.length - 1]; // Gets the last part which should be the folder name
-      const readmeUrl = `https://michaelakridge-noaa.github.io/container-images/images/${folderName}`; // Correctly constructs the URL
+        data.forEach(pkg => {
+            const parts = pkg.name.split('/'); // Splits the 'name' into parts
+            const folderName = parts[parts.length - 1]; // Gets the last part which should be the folder name
+            const readmeUrl = `https://michaelakridge-noaa.github.io/container-images/images/${folderName}`; // Correctly constructs the URL
 
-      const listItem = document.createElement('li');
-      listItem.className = 'dropdown-item'; // Class for dropdown items
-      const link = document.createElement('a');
-      link.href = readmeUrl;
-      link.textContent = folderName; // Displays the folder name as the link text
-      listItem.appendChild(link);
-      navList.appendChild(listItem); // Appends the list item to the dropdown menu
-    });
-  })
-  .catch(error => console.error('Failed to load packages:', error));
+            const listItem = document.createElement('li');
+            listItem.className = 'dropdown-item'; // Class for dropdown items
+            const link = document.createElement('a');
+            link.href = readmeUrl;
+            link.textContent = folderName; // Displays the folder name as the link text
+            listItem.appendChild(link);
+            navList.appendChild(listItem); // Appends the list item to the dropdown menu
+        });
+    })
+    .catch(error => console.error('Failed to load packages:', error));
+});
 
