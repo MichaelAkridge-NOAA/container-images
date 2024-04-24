fc1221bda4a8d3856c16823b5394b1b6eca4bc90
Michael Akridge <michael.akridge@noaa.gov>
2024-04-24 09:44:42 -1000

    dropdown nav test

--- assets/js/loadReadmes.js
+++ assets/js/loadReadmes.js
@@ -1,21 +1,20 @@
 fetch('packages.json')
   .then(response => response.json())
   .then(data => {
-    const navList = document.getElementById('packageList'); // Ensure this ID matches with your HTML
+    const navList = document.getElementById('dropdownMenu'); // This ID should refer to the dropdown menu container
 
     data.forEach(pkg => {
       const parts = pkg.name.split('/'); // Splits the 'name' into parts
       const folderName = parts[parts.length - 1]; // Gets the last part which should be the folder name
-      const readmeUrl = `https://michaelakridge-noaa.github.io/images/${folderName}`; // Correctly constructs the URL
+      const readmeUrl = `https://michaelakridge-noaa.github.io/container-images/images/${folderName}`; // Correctly constructs the URL
 
       const listItem = document.createElement('li');
-      listItem.className = 'nav-item'; // Adds Bootstrap or your custom class for styling
+      listItem.className = 'dropdown-item'; // Class for dropdown items
       const link = document.createElement('a');
       link.href = readmeUrl;
-      link.className = 'nav-link'; // Adds Bootstrap or your custom class for styling
       link.textContent = folderName; // Displays the folder name as the link text
       listItem.appendChild(link);
-      navList.appendChild(listItem); // Appends the list item to the navigation list
+      navList.appendChild(listItem); // Appends the list item to the dropdown menu
     });
   })
   .catch(error => console.error('Failed to load packages:', error));
